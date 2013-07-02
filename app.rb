4455ab2afaaa011d8388fc4af8e3b98990027d41
ashleygwilliams <heyashleyashley@gmail.com>
2013-07-02 09:47:51 -0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,42 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+Dir.glob('./lib/*.rb') do |model|
+  require model
+end
+
+module Citibike
+	class App < Sinatra::Application
+    before do
+      json = File.open("data/citibikenyc.json").read
+      @data = MultiJson.load(json)
+    end
+
+    get '/' do
+      erb :home
+    end
+
+    get '/map' do
+      erb :map
+    end
+
+    get '/form' do
+      erb :form
+    end
+
+    post '/form' do  
+      @start = @params[:start]
+      @end = @params[:end]
+      "you chose #{@start} and #{@end}"
+    end  
+
+    post '/map' do  
+      @start = @params[:start]
+      @end = @params[:end]
+      erb :map
+    end  
+
+  end
+end
